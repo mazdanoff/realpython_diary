13b02bce3080f859ed13369247055c0f032d85d0
Daniel <mazdanon@live.com>
2022-09-01 00:13:24 +0200

    Started project. Test outlines added.

---
+++ tests/test_diary_sanity.py
@@ -0,0 +1,24 @@
+# runserver fixture
+
+def test_login_page_loads_up():
+    # load up login page
+    # check if displayed
+    pass
+
+
+def test_login_required():
+    # try displaying any other page (create, read, update, delete)
+    # check if login page displayed
+    pass
+
+
+def test_diary_page_login_logout():
+    # load up page
+    # check if displayed
+    # enter admin:pass
+    # click login
+    # check if diary displayed
+    # click logout
+    # check if logged out page is displayed
+    pass
+
