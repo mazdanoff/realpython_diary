ff670bc02f812eae8457e9ed34c5941f02c1c146
Daniel Mazurkiewicz <mazdanon@live.com>
2025-02-01 00:45:02 +0100

    geckodriver is downloadable from a script

--- conf/paths.py
+++ conf/paths.py
@@ -3,6 +3,7 @@ import os
 project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 drivers_dir = os.path.join(project_path, "drivers")
-geckodriver = os.path.join(drivers_dir, "geckodriver.exe")
+geckodriver_zip = os.path.join(drivers_dir, "geckodriver.zip")
+geckodriver_exe = os.path.join(drivers_dir, "geckodriver.exe")
 
 database_path = os.path.join(project_path, "db.sqlite3")
--- conf/urls.py
+++ conf/urls.py
@@ -1,1 +1,3 @@
 MAIN_PAGE_URL = 'http://127.0.0.1:8000'
+
+GECKODRIVER_URL = "https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-win64.zip"
